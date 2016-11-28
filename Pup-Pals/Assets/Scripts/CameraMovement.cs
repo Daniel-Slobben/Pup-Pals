6719cdae33eda11d6055968b86198842437bf2d5
Daniël Slobben <d.j.slobben@st.hanze.nl>
2016-11-28 19:58:49 +0100

    Added scrolling and made sure the camera doesnt go of the edge of the map

--- Pup-Pals/Assets/Scripts/CameraMovement.cs
+++ Pup-Pals/Assets/Scripts/CameraMovement.cs
@@ -3,14 +3,27 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
+
     public float movementZone = 30;
-    public float movementSpeed = 0.5f;
+    public float movementSpeed = 0.1f;
 
     public float xMax = 11.37f;
     public float xMin = -11.28f;
 
     public float yMax = 4.83f;
     public float yMin = -4.96f;
+
+    public float mapX;
+    public float mapY;
+
+
+    private float zoom = 5;
+    private float zoomSpeed = 0.2f;
+    private float zoomMax = 10;
+    private float zoomMin = 2;
+
+    public SpriteRenderer spriteBounds;
+
 
     private Vector3 desiredPosition;
 
@@ -23,7 +36,30 @@ public class CameraMovement : MonoBehaviour {
     {
         float x = 0, y =0, z = 0;
         float speed = movementSpeed * Time.deltaTime;
+        
+        updateZoom();
 
+        // WASD/Arrow keys movement
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        // horizontal movement
+        if (horizontalAxis < 0)
+            x -= speed;
+        if (horizontalAxis > 0)
+            x += speed;
+        // vertical
+        if (verticalAxis < 0)
+            y -= speed;
+        if (verticalAxis > 0)
+            y += speed;
+
+        if (verticalAxis == 0)
+            y = 0;
+        if (horizontalAxis == 0)
+            x = 0;
+            
+        // mouse movement
         if (Input.mousePosition.x < movementZone)
         {
             x -= speed;
@@ -41,14 +77,67 @@ public class CameraMovement : MonoBehaviour {
         else if (Input.mousePosition.y > Screen.height - movementZone)
         {
             y += speed;
-        }
-            
+        }            
 
         Vector3 move = new Vector3(x, y, z) + desiredPosition;
-        move.x = Mathf.Clamp(move.x, xMin, xMax);
-        move.y = Mathf.Clamp(move.y, yMin, yMax);
-        desiredPosition = move;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
+
+        float vertExtent = Camera.main.orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+
+        float leftBound = horzExtent - mapX / 2;
+        float rightBound = mapX / 2 - horzExtent;
+        float bottomBound = vertExtent - mapY / 2;
+        float topBound = mapY / 2 - vertExtent;
+
+        float camX = Mathf.Clamp(move.x, leftBound, rightBound);
+        float camY = Mathf.Clamp(move.y, bottomBound, topBound);
+
+        if (camX == leftBound)
+            camX += 0.01f;
+        if (camX == rightBound)
+            camX -= 0.01f;
+        if (camY == bottomBound)
+            camY += 0.01f;
+        if (camY == topBound)
+            camY -= 0.01f;
+
+        var newCameraVector = new Vector3(camX, camY, Camera.main.transform.position.z);
+
+        if (Camera.main.transform.position.x != newCameraVector.x || Camera.main.transform.position.y != newCameraVector.y)
+        {
+            desiredPosition = newCameraVector;
+        }
+        Camera.main.transform.position = newCameraVector;
+        
 
     }
+
+    private void updateZoom()
+    {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            float tempZoom = zoom;
+            if (scroll > 0f)
+            {
+                // scroll up
+                tempZoom -= zoomSpeed;
+            }
+            else if (scroll < 0f)
+            {
+                // scroll down
+                tempZoom += zoomSpeed;
+            }
+            if (tempZoom < zoomMin || tempZoom > zoomMax)
+            {
+                // nothing
+            }
+            else
+            {
+                zoom = tempZoom;
+                Camera.main.orthographicSize = zoom;
+            }                
+        }        
+    }
+
 }
