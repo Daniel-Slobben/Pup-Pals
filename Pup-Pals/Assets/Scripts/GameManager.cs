026eac6821a3570d4f925a112392d1cfa0341f22
Marnix <marnimans@live.nl>
2016-11-30 15:00:27 +0100

    Fixed code structure, added game manager script (for turn ticks) and added new GUI features.

---
+++ Pup-Pals/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int soap = 0;
+    public int buildingMaterials = 0;
+    public int money = 0;
+    public int turnNumber = 0;
+
+    Text soapText;
+    Text buildingMaterialsText;
+    Text moneyText;
+    Text turnNumberText;
+
+    // Use this for initialization
+    void Start () {
+
+        soapText = GameObject.Find("SoapValue").GetComponent<Text>();
+        buildingMaterialsText = GameObject.Find("BuildMatsValue").GetComponent<Text>();
+        moneyText = GameObject.Find("MoneyValue").GetComponent<Text>();
+        turnNumberText = GameObject.Find("TurnValue").GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    //Is called when the "End turn" button is pressed.
+    public void nextTurn()
+    {
+        soap = soap + 5;
+        turnNumber = turnNumber + 1;
+        buildingMaterials = buildingMaterials + 3;
+        money = money + 5;
+
+        soapText.text = "" + soap;
+        buildingMaterialsText.text = "" + buildingMaterials;
+        moneyText.text = "" + money;
+        turnNumberText.text = "" + turnNumber;
+
+    }
+}
