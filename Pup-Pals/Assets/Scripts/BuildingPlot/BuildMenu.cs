d29a5db48d6413efcc805242dbbdeba643c7e95a
Daniël Slobben <d.j.slobben@st.hanze.nl>
2016-12-18 16:42:15 +0100

    Refactored building_plot. Still needs work on the visuals of it though.

---
+++ Pup-Pals/Assets/Scripts/BuildingPlot/BuildMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildMenu : MonoBehaviour {
+    public GameObject buildingPLot;
+    public GameObject text;
+    private string info;
+    private BuildingPlot buildingPlotScript;
+
+    // Use this for initialization
+    void Start () {
+        buildingPlotScript = (BuildingPlot)buildingPLot.GetComponent(typeof(BuildingPlot));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setInfo(string info)
+    {
+        if (info == "NotEnoughMoney")
+        {
+            text.GetComponent<TextMesh>().text = "Not enough money";
+        }
+        else
+        {
+            this.info = info;
+            string textToShow = "";
+            if (info == "Farm")
+            {
+                textToShow = info + System.Environment.NewLine + "Cost: " + Mathf.Abs(Farm.woodCost);
+            }
+            if (info == "Workshop")
+            {
+                textToShow = info + System.Environment.NewLine + "Cost: " + Mathf.Abs(Workshop.woodCost);
+            }
+            if (info == "School")
+            {
+                textToShow = info + System.Environment.NewLine + "Cost: " + Mathf.Abs(School.woodCost);
+            }
+            if (info == "Sanitation Building")
+            {
+                textToShow = info + System.Environment.NewLine + "Cost: " + Mathf.Abs(SanitationBuilding.woodCost);
+            }
+            text.GetComponent<TextMesh>().text = textToShow;
+        }
+
+    }
+    private void OnMouseDown()
+    {
+        //buildingPlotScript.build(info);
+        Debug.Log("builidng now: "+info);
+        buildingPlotScript.build(info);
+    }
+}
