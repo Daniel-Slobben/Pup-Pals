744cb2f95b5aa92e01b6fa96b94908187e579c9f
Marnix <marnimans@live.nl>
2017-01-19 16:48:37 +0100

    Added particles and sound

---
+++ Pup-Pals/Assets/Scripts/WashingMinigame/startParticles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startParticles : MonoBehaviour
+{
+
+    public float objectDistance = 5;
+    public AudioSource audioSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 pos = r.GetPoint(objectDistance);
+        transform.position = pos;
+
+        if (Input.GetMouseButton(0))
+        {
+        }
+        else
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            audioSource.Play();
+        }
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            audioSource.Stop();
+        }
+
+    }
+}
